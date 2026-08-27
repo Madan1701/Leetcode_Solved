7517d1be994fde9a03a80c940bd17ff65c3b1290
Madan <madanguna17@gmail.com>
2026-08-27 20:56:29 +0530

    Time: 1 ms (77.48%), Space: 43.5 MB (50.98%) - LeetHub

--- 3894-traffic-signal-color/3894-traffic-signal-color.java
+++ 3894-traffic-signal-color/3894-traffic-signal-color.java
@@ -1,14 +1,16 @@
 class Solution {
     public String trafficSignal(int timer) {
-        if(timer<=90 && timer>30){
-            return "Red";
-        }
-        else if(timer == 0){
+        
+        if(timer == 0){
             return "Green";
         }
         else if(timer==30){
             return "Orange";
         }
+        else if(timer<=90 && timer>30){
+            return "Red";
+        }
+        
         
         else{
             return "Invalid";
